ac82b678cc334f125bdb92a68de12f09ad470182
penidojoao <79549715+penidojoao@users.noreply.github.com>
2021-08-12 14:37:05 +0000

    Exercicio 2

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int t;
+   float v,r;
+  
+   scanf("%i %f %f", &t, &v, &r);
+   printf("R = %0.0f\n", r);
+   printf("V = %0.0f\n", v);
+   printf("T = %i\n", t);
+   printf("D = %0.0f\n", t*v);
+   printf("L = %0.0f\n", (t*v)/r);
    return 0;
 }
