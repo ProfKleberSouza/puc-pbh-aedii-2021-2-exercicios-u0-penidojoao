0dfe35662eeb5987084530d72a7b12f45506668e
penidojoao <79549715+penidojoao@users.noreply.github.com>
2021-08-16 15:56:10 +0000

    Exercicio 4

--- exe04/main.c
+++ exe04/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+   int a,b,c,maior,menor;
+   scanf("%i %i %i", &a, &b, &c);
+   maior = 0;
+   menor = 0;
+   if(a > b && a > c ){
+
+     maior = a;
+   }
+   if(b > a && b > c ){
+
+     maior = b;
+   }
+
+  if(c > a && c > b ){
+
+    maior = c;}
+
+  if(a < b && a < c ){
+
+
+    menor = a;
+  }  
+
+  if(b < a && b < c ){
+
+
+    menor = b;
+  }  
+
+  if(c < b && c < a ){
+
+
+    menor = c;
+  }  
+
+printf("MENOR = %i\n", menor);
+printf("MAIOR = %i\n", maior);
+
+
 }
