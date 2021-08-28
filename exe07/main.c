3c5850692076cb7ea75aba9eac6ecc8f176c58a2
penidojoao <79549715+penidojoao@users.noreply.github.com>
2021-08-28 01:07:44 +0000

    Exercicio 7

--- exe07/main.c
+++ exe07/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main (){
+
+int n,maior,menor;
+scanf("%i",&n);
+
+int vet[n];
+for(int i = 0;i<n;i++)
+{
+scanf("%i",&vet[i]);
+}
+
+maior = vet[0];
+menor = vet[0];
+
+for(int i=0;i<n;i++){
+
+
+
+if (vet[i] > maior)
+{
+  maior = vet[i];
+
+}
+
+if (vet[i] < menor)
+{
+ menor = vet[i];
+}
+
+
+}
+
+printf("MENOR = %i\n",menor);
+printf("MAIOR = %i\n",maior);
+
 }
