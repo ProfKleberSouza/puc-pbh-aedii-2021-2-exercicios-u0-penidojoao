61138275c37d655766050d8958bf3dd492d1a6b3
penidojoao <79549715+penidojoao@users.noreply.github.com>
2021-08-30 01:20:15 +0000

    Exercicio 8

--- exe08/main.c
+++ exe08/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+int main() 
+{
+   
+int n;
+
+while(n!=0)
+{
+  scanf("%i",&n);
+ 
+if(n>0){
+
+  printf("POSITIVO\n");
+} 
+
+else if(n<0){
+
+  printf("NEGATIVO\n");
+}
+
+}
+return 0;
 }
